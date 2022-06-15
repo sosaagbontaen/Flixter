6c0d0c352eb96f89354dab7c7b3fb44f3543df22
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-06-15 16:52:51 -0700

    Working on Step 2 Part 5 in viewDidLoad()

---
+++ Flixter/MovieViewController.h
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.h
+//  Flixter
+//
+//  Created by Samuel Osa-Agbontaen on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
