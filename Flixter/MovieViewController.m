9248d9f9016678f8c7704d05ba6642ae731c654a
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-06-16 15:54:20 -0700

    Added basic table view with dummy numbers

--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -7,16 +7,41 @@
 
 #import "MovieViewController.h"
 
-@interface MovieViewController ()
+@interface MovieViewController () <UITableViewDataSource, UITableViewDelegate>
+//remember when a property is set, you must call it with self.myArray
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+
 
 @end
 
 @implementation MovieViewController
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    //Returns the number of rows in your table view
+    return 90; //TODO : Change this after you create your cell!
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    //UITableView *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableView" forIndexPath:indexPath];
+    
+    NSLog(@"%@", [NSString stringWithFormat:@"row: %d, section %d", indexPath.row, indexPath.section]);
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"row: %d, section %d", indexPath.row, indexPath.section];
+    
+    return cell;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
     // Do any additional setup after loading the view.
-    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=712173aabafc3967756539ad38e6d2be"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
@@ -25,8 +50,14 @@
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-
+               //DEBUG: print the data dictionary to the console
+               //NSLog(@"%@", dataDictionary);
                // TODO: Get the array of movies
+               NSArray *dataArray = dataDictionary[@"results"];
+               //DEBUG: For loop through dataArray
+               for (NSString *result in dataArray) {
+                   //NSLog(@"%@",result);
+               }
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
            }
