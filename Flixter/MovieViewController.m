8a92a0777417194a0686e9f9e1d49c0a7c6a5909
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-06-17 14:35:49 -0700

    Implemented network error handling

--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -72,6 +72,15 @@
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
                NSLog(@"%@", [error localizedDescription]);
+               UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies"
+                                              message:@"The internet connection appears to be offline."
+                                              preferredStyle:UIAlertControllerStyleAlert];
+                
+               UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault
+                  handler:^(UIAlertAction * action) {}];
+                
+               [alert addAction:defaultAction];
+               [self presentViewController:alert animated:YES completion:nil];
            }
            else {
                
